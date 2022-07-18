e19615461705e1a4555a879ec17904589620bf6d
asus <asus@DESKTOP-HMSSTKG>
2022-07-18 15:39:45 +0530

    done

--- lohith/src/lohith/lojitj.java
+++ lohith/src/lohith/lojitj.java
@@ -5,7 +5,7 @@ public class lojitj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("i ama lohith");
-		System.out.println("i ama lohith");
+	
 	}
 
 }
