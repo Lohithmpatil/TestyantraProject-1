acbab2b948c0bc5837ad1488e36b492a161ac3c5
iamsachin <iamsachin@DESKTOP-8RI6OF2>
2022-07-18 14:33:39 +0530

    commiting

--- com.crm.practice.project/src/test/java/com/crm/campaign/done_and_ok.java
+++ com.crm.practice.project/src/test/java/com/crm/campaign/done_and_ok.java
@@ -3,16 +3,18 @@ package com.crm.campaign;
 import org.testng.annotations.Test;
 
 public class done_and_ok {
-    @Test
-	public static void main(String[] args) {
+	@Test
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-System.out.println("done");
+		System.out.println("done...");
 
 
 	}
-    @Test
-    
-public void ok() {
-	System.out.println("ok");
-}
+	@Test
+
+	public void ok()
+	{
+		System.out.println("ok");
+	}
 }
