11956d6ffbc1243c5a558ca21d74b66c4cd0ee5f
asus <asus@DESKTOP-HMSSTKG>
2022-07-18 12:49:26 +0530

    done frame work

---
+++ com.crm.practice.project/src/test/java/com/crm/campaign/CreateCampaign.java
@@ -0,0 +1,18 @@
+package com.crm.campaign;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaign 
+{
+	@Test
+	public void campaignName()
+	{
+		System.out.println("where are u");
+		
+	}
+	@Test
+	public void doneAndDusted() 
+	{
+		System.out.println("all heavy");
+	}
+}
